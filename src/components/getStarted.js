fbf719f16dc33d5c6f43f9a9f82fcdcc3ced9313
Abhishek Joshi <abbu.joshi.satya@gmail.com>
2022-07-09 17:51:54 +0530

    added git link

--- src/components/getStarted.js
+++ src/components/getStarted.js
@@ -5,9 +5,11 @@ export default function Starter(){
 			<div className="description">
 					Build your resume with resume builder and give a new flight to come one step closer to your dream job.<br />
 					Resume builder is an open source project and you can contribute to it on github.
-				<div className="gitIcon">
-					<img src="./gitIco.svg" /> <div>Contribute on GitHub</div>
-				</div>
+				<a href="https://github.com/abhij2127/resume-builder.git" className="gitLink" target="_blank">
+					<div className="gitIcon">
+						<img src="./gitIco.svg" /> <div>Contribute on GitHub</div>
+					</div>
+				</a>
 				<div className="fireUp-container">
 					<a href="#createCV" className="fire-up">
 						Get Started
