96e9bd3210a634b8f84320b8e391205a214f0743
abhij2127 <55267403+abhij2127@users.noreply.github.com>
2022-07-20 10:53:07 +0530

    created user-nput form and form-groups with styles🐢🐢🎉

--- src/components/resume-form.js
+++ src/components/resume-form.js
@@ -1,7 +1,28 @@
 export default function ResumeForm(){
+	function getDetails(){
+		console.log("Getting the details and processing!")
+	}
 	return(
 		<div className="resume-form-container" id="createCV">
-			<h1>Hellow</h1>
+			<div className="form-sub-container">
+				<h1 className="form-title">Please fill the form below</h1>
+				<p className="form-subtitle">Let's Get Started with making your perfect resume</p>
+				<form id="applicant-details-form" onSubmit={getDetails}>
+					<div class="form-group">
+						<label>Name</label>
+						<input type="text" class="form-input" />
+					</div>
+					<div class="form-group">
+						<label>Email</label>
+						<input type="text" class="form-input" />
+					</div>
+					<div class="form-group">
+						<label>Phone no.</label>
+						<input type="text" class="form-input" />
+					</div>
+				
+				</form>
+			</div>
 		</div>
 	);
 }
